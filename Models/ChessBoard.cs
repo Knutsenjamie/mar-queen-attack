277920e3124f84a74caa14cc6c4dc5e2b8d4e7f7
Michael Burton <micbur1@gmail.com>
2021-05-03 13:46:46 -0700

    :sparkles: Created opponent constructor
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- Models/ChessBoard.cs
+++ Models/ChessBoard.cs
@@ -2,5 +2,14 @@ using System;
 
 namespace ChessBoard
 {
-
+  public class Opponent
+  {
+    
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Opponent(int x, int y) {
+      X = x;
+      Y = y;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using System;
 
-namespace Queen
+namespace ChessBoard
 {
   public class Program
   {
     public static void Main()
     {
-
+      Console.WriteLine("Welcome to a game of chess!");
+      Opponent opponent = new Opponent(1, 1);
     }
   }
 }
